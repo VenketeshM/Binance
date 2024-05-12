7f115ee9e9e11b24e8fe4e3ce79b95c6f652fd51
VENKETESH <hareeshkinikk@gmail.com>
2024-05-12 18:35:52 +0530

    trade bot ideas

--- trade_btc.py
+++ trade_btc.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 from keys import key, secret
 from binance.um_futures import UMFutures
 import ta
@@ -8,88 +7,133 @@ import pandas as pd
 from time import sleep
 from binance.error import ClientError
 
-# Initialize the Binance client
 client = UMFutures(key=key, secret=secret)
 
-# Constants for trading
-tp = 0.010  # 1 percent
-sl = 0.005  # 0.5 percent
-volume = 10  # Volume for one order
+tp = 0.005  # 1 percent
+sl = 0.001  # 0.5 percent
+volume = 50  # volume for one order (if its 10 and leverage is 10, then you put 1 usdt to one position)
 leverage = 10
-margin_type = 'ISOLATED'  # 'ISOLATED' or 'CROSS'
+type = 'ISOLATED'  # type is 'ISOLATED' or 'CROSS'
 qty = 1  # Amount of concurrent opened positions
-symbol = 'BTCUSDC'  # Symbol for trading
 
-# Function to get the trading balance
-def get_trading_balance(trading_active):
+current_trade_open = False
+symbol = ''
+
+# getting your futures balance in USDT
+def get_balance_usdt():
     try:
         response = client.balance(recvWindow=6000)
         for elem in response:
             if elem['asset'] == 'USDT':
-                balance = float(elem['balance'])
-                if trading_active:
-                    return balance / 2  # Using only half of the balance during trading
-                else:
-                    return balance
+                return float(elem['balance'])
     except ClientError as error:
         print("Error occurred:", error)
 
-# Function to get the price precision of a symbol
-def get_price_precision(symbol):
-    symbols_info = client.exchange_info()['symbols']
-    for symbol_info in symbols_info:
-        if symbol_info['symbol'] == symbol:
-            return symbol_info['pricePrecision']
+# Getting candles for the needed symbol
+def klines(symbol):
+    try:
+        resp = pd.DataFrame(client.klines(symbol, '15m'))
+        resp = resp.iloc[:, :6]
+        resp.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+        resp = resp.set_index('Time')
+        resp.index = pd.to_datetime(resp.index, unit='ms')
+        resp = resp.astype(float)
+        return resp
+    except ClientError as error:
+        print("Error occurred:", error)
 
-# Function to get the quantity precision of a symbol
-def get_qty_precision(symbol):
-    symbols_info = client.exchange_info()['symbols']
-    for symbol_info in symbols_info:
-        if symbol_info['symbol'] == symbol:
-            return symbol_info['quantityPrecision']
+# Set leverage for the needed symbol
+def set_leverage(symbol, level):
+    try:
+        response = client.change_leverage(
+            symbol=symbol, leverage=level, recvWindow=6000
+        )
+        print(response)
+    except ClientError as error:
+        print("Error occurred:", error)
 
-# Function to open a new order with the last price, and set TP and SL
-def open_order(symbol, side, balance):
+# The same for the margin type
+def set_mode(symbol, type):
+    try:
+        response = client.change_margin_type(
+            symbol=symbol, marginType=type, recvWindow=6000
+        )
+        print(response)
+    except ClientError as error:
+        print("Error occurred:", error)
+
+# Open new order with the last price, and set TP and SL
+def open_order(symbol, side):
     price = float(client.ticker_price(symbol)['price'])
-    qty_precision = get_qty_precision(symbol)
-    price_precision = get_price_precision(symbol)
-    qty = round(min(volume / price, balance), qty_precision)
+    qty_precision = 3  # BTCUSDC has 3 qty precision
+    price_precision = 2  # BTCUSDC has 2 price precision
+    qty = round(volume / price, qty_precision)
     if side == 'buy':
         try:
-            resp1 = client.new_order(symbol=symbol, side='BUY', type='MARKET', quantity=qty)
+            resp1 = client.new_order(symbol=symbol, side='BUY', type='LIMIT', quantity=qty, timeInForce='GTC',
+                                     price=price)
             print(symbol, side, "placing order")
             print(resp1)
             sleep(2)
             sl_price = round(price - price * sl, price_precision)
-            resp2 = client.new_order(symbol=symbol, side='SELL', type='STOP_MARKET', quantity=qty,
+            resp2 = client.new_order(symbol=symbol, side='SELL', type='STOP_MARKET', quantity=qty, timeInForce='GTC',
                                      stopPrice=sl_price)
             print(resp2)
             sleep(2)
             tp_price = round(price + price * tp, price_precision)
             resp3 = client.new_order(symbol=symbol, side='SELL', type='TAKE_PROFIT_MARKET', quantity=qty,
-                                     stopPrice=tp_price)
+                                     timeInForce='GTC', stopPrice=tp_price)
             print(resp3)
         except ClientError as error:
             print("Error occurred:", error)
     if side == 'sell':
         try:
-            resp1 = client.new_order(symbol=symbol, side='SELL', type='MARKET', quantity=qty)
+            resp1 = client.new_order(symbol=symbol, side='SELL', type='LIMIT', quantity=qty, timeInForce='GTC',
+                                     price=price)
             print(symbol, side, "placing order")
             print(resp1)
             sleep(2)
             sl_price = round(price + price * sl, price_precision)
-            resp2 = client.new_order(symbol=symbol, side='BUY', type='STOP_MARKET', quantity=qty,
+            resp2 = client.new_order(symbol=symbol, side='BUY', type='STOP_MARKET', quantity=qty, timeInForce='GTC',
                                      stopPrice=sl_price)
             print(resp2)
             sleep(2)
             tp_price = round(price - price * tp, price_precision)
             resp3 = client.new_order(symbol=symbol, side='BUY', type='TAKE_PROFIT_MARKET', quantity=qty,
-                                     stopPrice=tp_price)
+                                     timeInForce='GTC', stopPrice=tp_price)
             print(resp3)
         except ClientError as error:
             print("Error occurred:", error)
 
-# Function to check the RSI signal for a given symbol
+# Your current positions (returns the symbols list)
+def get_pos():
+    try:
+        resp = client.get_position_risk()
+        pos = []
+        for elem in resp:
+            if float(elem['positionAmt']) != 0:
+                pos.append(elem['symbol'])
+        return pos
+    except ClientError as error:
+        print("Error occurred:", error)
+
+def check_orders():
+    try:
+        response = client.get_orders(recvWindow=6000)
+        sym = []
+        for elem in response:
+            sym.append(elem['symbol'])
+        return sym
+    except ClientError as error:
+        print("Error occurred:", error)
+
+def close_open_orders(symbol):
+    try:
+        response = client.cancel_open_orders(symbol=symbol, recvWindow=6000)
+        print(response)
+    except ClientError as error:
+        print("Error occurred:", error)
+
 def str_rsi_signal(symbol):
     kl = klines(symbol)
     rsi = ta.momentum.RSIIndicator(kl.Close).rsi()
@@ -114,39 +158,41 @@ def str_rsi_signal(symbol):
     else:
         return 'none'
 
-# Function to get candles for the needed symbol
-def klines(symbol):
-    try:
-        resp = pd.DataFrame(client.klines(symbol, '15m'))
-        resp = resp.iloc[:, :6]
-        resp.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
-        resp = resp.set_index('Time')
-        resp.index = pd.to_datetime(resp.index, unit='ms')
-        resp = resp.astype(float)
-        return resp
-    except ClientError as error:
-        print("Error occurred:", error)
-
-# Main trading loop
-trading_active = False  # Initialize trading status
 while True:
-    balance = get_trading_balance(trading_active)
+    balance = get_balance_usdt()
     sleep(1)
     if balance is None:
         print('Cannot connect to API. Check IP, restrictions, or wait some time')
     if balance is not None:
         print("My balance is: ", balance, " USDT")
+        pos = get_pos()
+        print(f'You have {len(pos)} opened positions:\n{pos}')
+        ord = check_orders()
+        for elem in ord:
+            if elem not in pos:
+                close_open_orders(elem)
 
-        signal = str_rsi_signal(symbol)
-        if signal != 'none':
-            print(f'Found {signal.upper()} signal for {symbol}')
-            trading_active = True  # Activate trading
-            try:
-                open_order(symbol, 'buy' if signal == 'up' else 'sell', balance)
-            except ClientError as error:
-                print("Error occurred:", error)
-        else:
-            trading_active = False  # Deactivate trading
-
-    print('Waiting for the next trading opportunity...')
+        if not current_trade_open:
+            if len(pos) < qty:
+                symbol = 'BTCUSDC'
+                signal = str_rsi_signal(symbol)
+                if signal == 'up' and symbol not in pos and symbol not in ord:
+                    print('Found BUY signal for ', symbol)
+                    set_mode(symbol, type)
+                    sleep(1)
+                    set_leverage(symbol, leverage)
+                    sleep(1)
+                    print('Placing order for ', symbol)
+                    open_order(symbol, 'buy')
+                    current_trade_open = True
+                elif signal == 'down' and symbol not in pos and symbol not in ord:
+                    print('Found SELL signal for ', symbol)
+                    set_mode(symbol, type)
+                    sleep(1)
+                    set_leverage(symbol, leverage)
+                    sleep(1)
+                    print('Placing order for ', symbol)
+                    open_order(symbol, 'sell')
+                    current_trade_open = True
+    print('Waiting')
     sleep(60)
